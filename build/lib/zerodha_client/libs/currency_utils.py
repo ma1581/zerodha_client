83f2ba0b2bcf304ead3000fe68f07df5d769d54a
MA <mohammedanas1581@gmail.com>
2025-02-08 23:13:37 +0530

    removed ignorables

--- build/lib/zerodha_client/libs/currency_utils.py
+++
@@ -1,6 +0,0 @@
-class CurrencyUtils:
-    @classmethod
-    def format_currencies(cls, amount_string: str) -> float:
-        current_amount = amount_string.replace(",", "")
-        current_amount = current_amount.replace("₹", "")
-        return float(current_amount)
--- build/lib/zerodha_client/models/dashboard.py
+++
@@ -1,7 +0,0 @@
-class DashboardDetails:
-    investment_amount = None
-    current_value = None
-
-    def __init__(self, investment_amount, current_value):
-        self.investment_amount = investment_amount
-        self.current_value = current_value
